3c6ac3a79b72d87c88573040555c71533d288a49
Nathanael Yang <ny83427@gmail.com>
2019-05-10 23:44:55 -0700

    优化BFS内存使用及运行速度，从71秒到5秒，不OOM

--- src/main/java/MissionTrip.java
+++ src/main/java/MissionTrip.java
@@ -3,22 +3,6 @@ import java.util.Queue;
 
 public class MissionTrip {
 
-    private static class Node {
-        private int r, c;
-
-        private Node(int r, int c) {
-            this.r = r;
-            this.c = c;
-        }
-
-        @Override
-        public String toString() {
-            return String.format("[%d, %d]", r, c);
-        }
-    }
-
-    private boolean debug;
-
     /**
      * 计算从起点到终点所有的可能走法总数
      *
@@ -28,54 +12,36 @@ public class MissionTrip {
         final long now = System.currentTimeMillis();
         int sum = 0;
         int r = paths.length, c = paths[0].length;
-        Node root = new Node(0, 0);
-
-        Queue<Node> nodeQ = new LinkedList<>();
-        Queue<StringBuilder> pathQ = new LinkedList<>();
-        Queue<boolean[][]> visitQ = new LinkedList<>();
-        nodeQ.offer(root);
-        pathQ.offer(new StringBuilder());
-        visitQ.offer(new boolean[r][c]);
+        Queue<Integer> nodeQ = new LinkedList<>();
+        Queue<boolean[]> visitQ = new LinkedList<>();
+        nodeQ.offer(0);
+        visitQ.offer(new boolean[r * c]);
 
         int[] x = {-1, 1, 0, 0};
         int[] y = {0, 0, -1, 1};
-        char[] dirs = {'左', '右', '上', '下'};
         while (!nodeQ.isEmpty()) {
-            Node node = nodeQ.poll();
-            boolean[][] visit = visitQ.poll();
-            StringBuilder sb = pathQ.poll();
-            if (node == null || visit == null || sb == null || visit[node.r][node.c])
+            Integer node = nodeQ.poll();
+            boolean[] visit = visitQ.poll();
+            if (node == null || visit == null || visit[node])
                 continue;
 
-            visit[node.r][node.c] = true;
-            sb.append(node.toString());
-            if (node.r == r - 1 && node.c == c - 1) {
-                if (debug) System.out.println(sb);
+            visit[node] = true;
+            int nr = node / c, nc = node % c;
+            if (nr == r - 1 && nc == c - 1) {
                 sum++;
                 continue;
-            } else if (paths[node.r][node.c] == 1) {
-                sb.append("!敌人据点!");
+            } else if (paths[nr][nc] == 1) {
                 continue;
             }
 
             for (int i = 0; i < x.length; i++) {
-                int nr = node.r + y[i], nc = node.c + x[i];
-                if (nr < 0 || nr == r || nc < 0 || nc == c || visit[nr][nc])
+                int cr = nr + y[i], cc = nc + x[i];
+                if (cr < 0 || cr >= r || cc < 0 || cc >= c ||
+                    visit[cr * c + cc] || paths[cr][cc] == 1)
                     continue;
-                nodeQ.offer(new Node(nr, nc));
-                boolean[][] nVisit = new boolean[r][c];
-                for (int j = 0; j < r; j++) {
-                    nVisit[j] = visit[j].clone();
-                }
-                visitQ.offer(nVisit);
-                pathQ.offer(new StringBuilder(sb).append(" -")
-                    .append(dirs[i]).append("-> "));
+                nodeQ.offer(cr * c + cc);
+                visitQ.offer(visit.clone());
             }
-        }
-
-        if (debug) {
-            System.out.printf("Sum: %d. Time Spent: %.2fS%n", sum,
-                (System.currentTimeMillis() - now) / 1000.0);
         }
         return sum;
     }
@@ -88,13 +54,15 @@ public class MissionTrip {
         map[1][2] = map[2][1] = 1;
         mt.resolve(map);
 
-        mt.debug = true;
         map[2][1] = 0;
         mt.resolve(map);
 
-        mt.debug = false;
         mt.resolve(new int[2][2]);
-        mt.resolve(new int[4][4]);
-        mt.resolve(new int[5][5]);
+        for (int i = 4; i <= 6; i++) {
+            final long now = System.currentTimeMillis();
+            long sum = mt.resolve(new int[i][i]);
+            System.out.printf("Size: %d, Sum: %d. Time Spent: %.2fS%n", sum,
+                i, (System.currentTimeMillis() - now) / 1000.0);
+        }
     }
 }
