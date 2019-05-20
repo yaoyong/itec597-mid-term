b61e17983a1ddda0fd5316d5b9a217cf0b172a34
Nathanael Yang <ny83427@gmail.com>
2019-05-19 17:00:41 -0700

    追加DFS实现，补充原题链接方便学生参考

--- src/main/java/MissionTrip.java
+++ src/main/java/MissionTrip.java
@@ -3,8 +3,39 @@ import java.util.Queue;
 
 public class MissionTrip {
 
+    private int count;
+
     /**
-     * 计算从起点到终点所有的可能走法总数
+     * 可参考：https://leetcode.com/problems/unique-paths-iii/
+     * 该题要求穿过所有非障碍点且仅穿过一次，因而还需要统计穿过的数量
+     * Size: 6, Sum: 1262816. Time Spent: 0.47S
+     * Size: 7, Sum: 575780564. Time Spent: 279.35S
+     */
+    private int resolveDFS(int[][] paths) {
+        count = 0;
+        dfs(paths, 0, 0, new boolean[paths.length][paths[0].length]);
+        return count;
+    }
+
+    private void dfs(int[][] paths, int r, int c, boolean[][] marks) {
+        if (r < 0 || r >= paths.length || c < 0 || c >= paths[0].length ||
+            paths[r][c] == 1 || marks[r][c]) {
+            return;
+        } else if (r == paths.length - 1 && c == paths[0].length - 1) {
+            count++;
+            return;
+        }
+
+        marks[r][c] = true;
+        for (int i = 0; i < rOffsets.length; i++)
+            dfs(paths, r + rOffsets[i], c + cOffsets[i], marks);
+        marks[r][c] = false;
+    }
+
+    private final int[] rOffsets = {-1, 1, 0, 0}, cOffsets = {0, 0, -1, 1};
+
+    /**
+     * 计算从起点到终点所有的可能走法总数，采用相对直观的BFS处理方法
      *
      * @param paths 旅途，当某处值为1时不允许走过，为0时可以正常走过
      */
@@ -17,8 +48,6 @@ public class MissionTrip {
         nodes.offer(0);
         visited.offer(new boolean[r * c]);
 
-        int[] x = {-1, 1, 0, 0};
-        int[] y = {0, 0, -1, 1};
         while (!nodes.isEmpty()) {
             Integer node = nodes.poll();
             boolean[] visit = visited.poll();
@@ -32,8 +61,8 @@ public class MissionTrip {
                 continue;
             }
 
-            for (int i = 0; i < x.length; i++) {
-                int cr = nr + y[i], cc = nc + x[i];
+            for (int i = 0; i < rOffsets.length; i++) {
+                int cr = nr + rOffsets[i], cc = nc + cOffsets[i];
                 if (cr < 0 || cr >= r || cc < 0 || cc >= c ||
                     visit[cr * c + cc] || paths[cr][cc] == 1)
                     continue;
@@ -62,5 +91,13 @@ public class MissionTrip {
             System.out.printf("Size: %d, Sum: %d. Time Spent: %.2fS%n", i,
                 sum, (System.currentTimeMillis() - now) / 1000.0);
         }
+
+        for (int i = 6; i <= 7; i++) {
+            final long now = System.currentTimeMillis();
+            long sum = mt.resolveDFS(new int[i][i]);
+            System.out.printf("Size: %d, Sum: %d. Time Spent: %.2fS%n", i,
+                sum, (System.currentTimeMillis() - now) / 1000.0);
+        }
     }
+
 }
