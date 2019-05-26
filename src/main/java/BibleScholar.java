d4eec5a4f1ab6a62d091c3a62133620db6560cb0
yaoyong <yaoyong0905@gmail.com>
2019-05-26 14:57:04 -0700

    Update BibleScholar.java

--- src/main/java/BibleScholar.java
+++ src/main/java/BibleScholar.java
@@ -1,4 +1,18 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Stream;
+
 public class BibleScholar {
+
+    private static final int COUNT = 12;
 
     /**
      * <pre>
@@ -8,8 +22,72 @@ public class BibleScholar {
      * ......
      * </pre>
      */
-    public String[] resolve() {
-        return null;
+    public String[] resolve() throws FileNotFoundException{
+        Set<String> stopWords = new HashSet<>();
+
+        try (Scanner scw = new Scanner(getClass().getResourceAsStream("main/resources/stopwords.txt"))) {
+            while (scw.hasNextLine())
+                stopWords.add(scw.nextLine().trim());
+        }
+
+        Map<String, Integer> map = new HashMap<>();
+        try (Scanner scb = new Scanner(getClass().getResourceAsStream("main/resources/kjv.txt"))) {
+            while (scb.hasNext()) {
+                String word = scb.next();
+                int len = word.length(), begin = 0, end = len - 1;
+                while (begin < len && !Character.isAlphabetic(word.charAt(begin)))
+                    begin++;
+                if (begin == len)
+                    continue;
+
+                while (end > 0 && !Character.isAlphabetic(word.charAt(end)))
+                    end--;
+                String converted = word.substring(begin, end + 1).toLowerCase();
+                if (stopWords.contains(converted))
+                    continue;
+
+                map.put(converted, map.getOrDefault(converted, 0) + 1);
+            }
+        }
+
+        Comparator<Entry<String, Integer>> comp = (a, b) -> a.getValue().equals(b.getValue()) ?
+            a.getKey().compareTo(b.getKey()) : a.getValue() - b.getValue();
+        PriorityQueue<Entry<String, Integer>> minPQ = new PriorityQueue<>(COUNT, comp);
+        PriorityQueue<Entry<String, Integer>> maxPQ = new PriorityQueue<>(COUNT, comp.reversed());
+        for (Entry<String, Integer> entry : map.entrySet()) {
+            if (minPQ.size() < COUNT) {
+                minPQ.offer(entry);
+            } else if (comp.compare(entry, minPQ.peek()) > 0) {
+                minPQ.poll();
+                minPQ.offer(entry);
+            }
+
+            if (maxPQ.size() < COUNT) {
+                maxPQ.offer(entry);
+            } else if (comp.compare(entry, maxPQ.peek()) < 0) {
+                maxPQ.poll();
+                maxPQ.offer(entry);
+            }
+        }
+
+        final String[] result = new String[COUNT * 2];
+        for (int i = COUNT - 1; i >= 0; i--) {
+            Entry<String, Integer> entry = minPQ.poll();
+            if (entry == null) break;
+            result[i] = entry.getKey() + ":" + entry.getValue();
+        }
+
+        for (int i = COUNT; i < COUNT * 2; i++) {
+            Entry<String, Integer> entry = maxPQ.poll();
+            if (entry == null) break;
+            result[i] = entry.getKey() + ":" + entry.getValue();
+        }
+        return result;
     }
 
+    public static void main(String[] args) throws FileNotFoundException {
+        final long now = System.currentTimeMillis();
+        Stream.of(new BibleScholar().resolve()).forEach(System.out::println);
+        System.out.printf("Time Spent: %.2fS%n", (System.currentTimeMillis() - now) / 1000.0);
+    }
 }
